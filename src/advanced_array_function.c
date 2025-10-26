4190cbb67ee7eead6cf5933008d02180d2053fb3
Петухов Артём Эдуардович ИП-512 <artempetuhov123@yandex.ru>
2025-10-26 15:42:55 +0700

    готовы 1 и 2

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -1,14 +1,44 @@
 #include "../include/advanced_array_function.h"
 
 int max_subarray_sum(int* nums, int size) {
-    return 0;
+  int msum = nums[0];
+  int sum = nums[0];
+    for (int i = 1; i < size; i++){
+        if (sum < 0){
+            sum = nums[i];
+        }else{
+            sum += nums[i];
+        }
+        if (sum > msum){
+            msum = sum;
+        }
+
+    }
+    return msum;
 }
 
 int length_of_lis(int* nums, int numsSize) {
-    return 0;
+  if (numsSize == 0) return 0;
+  int mlen = 1;
+  int len = 1;
+    for (int i = 1; i < numsSize; i++){
+      if (nums[i] > nums[i-1]){
+        len++;
+      }else{
+        len = 1;
+      }
+      if (len > mlen){
+        mlen = len;
+      }
+
+    }
+    return mlen;
 }
 
+
+
 int* merge(int* intervals, int intervalsSize, int* returnSize) {
-    return NULL;
+
 }
+
 
